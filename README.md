74ec67fd15272ddd1f5fed38e208d93948696ea4
CbS Ghost <cbsghost@gmail.com>
2017-07-23 06:32:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# fish-legacycolor
+
+Tweak the color scheme of [fish-shell](https://github.com/fish-shell/fish-shell) back version 2.2 to make some themes looking nice again.
+
+## Install
+> Note: fish version >=2.3.0 is required.
+
+With [fisherman]
+
+```
+fisher cbsghost/fish-legacycolor
+```
+
+[fisherman]: https://github.com/fisherman/fisherman
+
+## License
+This is free and unencumbered software released into the public domain.  
+For more information, please refer to <http://unlicense.org/>
---
+++ conf.d/legacycolor.fish
@@ -0,0 +1,36 @@
+# Regular syntax highlighting colors
+set -g fish_color_normal normal
+set -g fish_color_command 005fd7 purple
+set -g fish_color_param 00afff cyan
+set -g fish_color_redirection normal
+set -g fish_color_comment red
+set -g fish_color_error red --bold
+set -g fish_color_escape cyan
+set -g fish_color_operator cyan
+set -g fish_color_quote brown
+set -g fish_color_autosuggestion 555 yellow
+set -g fish_color_valid_path --underline
+
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+
+# Background color for matching quotes and parenthesis
+set -g fish_color_match cyan
+
+# Background color for search matches
+set -g fish_color_search_match --background=purple
+
+# Background color for selections
+set -g fish_color_selection --background=purple
+
+# Pager colors
+set -g fish_pager_color_prefix cyan
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description 555 yellow
+set -g fish_pager_color_progress cyan
+
+#
+# Directory history colors
+#
+
+set -g fish_color_history_current cyan
